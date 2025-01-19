cbf4dc2199a8193ad83db58b013f56fd26224e0a
zahrafeisal <zahrafeisal3@gmail.com>
2025-01-19 21:39:06 +0300

    attributes and methods

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
+
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Old"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if type(size) in (int, float):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
     pass
